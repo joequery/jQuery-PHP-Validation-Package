2d19c3e1b99f92942c5f807b8723e97c6d63f0ad
Joseph McCullough <joseph@vertstudios.com>
2011-05-23 13:08:10 -0500

    Finalized GetSet js test

--- js/tdd/GetSet/GetSet_test.js
+++ js/tdd/GetSet/GetSet_test.js
@@ -1,10 +1,13 @@
-var gs = new GetSet();
+$(document).ready(function(){
+	var gs = new GetSet();
 
-var woohoo={
-	key: "value1",
-	key2: "value2"
-}
+	var woohoo= {
+		key: "value1",
+		key2: "value2"
+	}
 
-gs.getters({scope: window, obj: woohoo});
-$("#main").append(window.getKey() + "<br />");
-$("#main").append(window.getKey2() + "<br />");
+	gs.getters({scope: window, obj: woohoo});
+
+	$("#main").append(window.getKey() + "<br />");
+	$("#main").append(window.getKey2() + "<br />");
+});
